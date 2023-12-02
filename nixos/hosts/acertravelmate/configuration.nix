cdab1a5d6356b2bd3a8d7f01f7a7ff34a13093a8
Der Beta Kevin <derbetakevin@outlook.de>
2023-12-02 17:16:45 +0100

    Add acertravelmate

---
+++ nixos/hosts/acertravelmate/configuration.nix
@@ -0,0 +1,70 @@
+# acertravelmate specific config
+{ config, pkgs, ... }: {
+  
+  # Imports specific
+  imports = [
+    ../../kde.nix
+  ];
+
+  # Bootloader has to be done per machine, since the Acer doesn't 
+  # support UEFI.
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+      };
+    };
+
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  # Hostname, networking and bluetooth
+  networking = {
+    # Specify hostname
+    hostName = "AcerNix2306-1";
+  };
+  
+  # Services are good. Services are great. We should use them!
+  services = {
+    
+    # Enable printing service via CUPS
+    printing = {
+      enable = true;
+      # Add drivers for Epson WF-7710 printer support
+      drivers = with pkgs; [
+        epson-escpr2
+      ];
+    };
+
+    # Add JACK support to PipeWire
+    pipewire = {
+      jack.enable = false;
+    };
+  };
+
+  # User account related things specific
+  users = {
+    users = {
+      derbetakevin = {
+        extraGroups = [];
+        shell = pkgs.zsh;
+      };
+    };
+
+    groups = {
+      libvirtd = {
+        members = ["root" "derbetakevin"];
+      };
+    };
+  };
+
+  # Environment related things specific
+  environment = {
+    
+    # Packages specific
+    systemPackages = with pkgs; [
+      #No specific packages
+    ];
+  };
+}
