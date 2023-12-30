c4fe395f5235246e67c25e5e24b7a868ffd94178
Der Beta Kevin <derbetakevin@outlook.de>
2023-12-30 20:26:49 +0100

    Symlink files from other locations

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,5 +1,29 @@
+# Home Manager Configuration for all devices
+
 { config, lib, pkgs, ... }:
 
 {
-
+  home = {
+    packages = with pkgs; [
+    ];
+    # Symlink files from other locations
+    file = {
+      #".config/alacritty" = {
+      #  source = config.lib.file.mkOutOfStoreSymlink "/home/derbetakevin/Development/dotfiles/Applications/alacritty";
+      #  recursive = true;
+      #};
+      ".config/conky" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/home/derbetakevin/Development/dotfiles/Applications/conky";
+        recursive = true;
+      };
+      ".config/fastfetch" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/home/derbetakevin/Development/dotfiles/Applications/fastfetch";
+        recursive = true;
+      };
+      ".config/neofetch" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/home/derbetakevin/Development/dotfiles/Applications/neofetch";
+        recursive = true;
+      };
+    };
+  };
 }
